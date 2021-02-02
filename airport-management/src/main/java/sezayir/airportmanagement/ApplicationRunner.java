8698c778e4981f199a3c43a72bad4c1e8561e3e3
Sezayir Dağtekin <sezayir@hotmail.com>
2021-02-02 14:53:24 +0300

    connect db using application properties and create db with mongodb template

--- airport-management/src/main/java/sezayir/airportmanagement/ApplicationRunner.java
+++ airport-management/src/main/java/sezayir/airportmanagement/ApplicationRunner.java
@@ -1,17 +1,30 @@
 package sezayir.airportmanagement;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Component;
 
 import lombok.extern.java.Log;
+import sezayir.airportmanagement.domain.FlightInformation;
 
 @Component
 @Log
 public class ApplicationRunner implements CommandLineRunner {
 
+	private MongoTemplate template;
+
+	ApplicationRunner(MongoTemplate template) {
+		this.template = template;
+	}
+
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Application started...");
+
+		FlightInformation flight = new FlightInformation();
+		template.save(flight);
+		log.info("FlightInformation saved...");
+
 	}
 
 }
